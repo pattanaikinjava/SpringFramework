2be5fc966d98740184a4fc6313838845e7d2a24c
pattanaikinjava <pattanaikd1998@gmail.com>
2023-02-25 19:19:02 +0530

    constructor inject

---
+++ 05-SC-ConstructorInjection/src/main/java/in/company/network/NetworkService.java
@@ -0,0 +1,24 @@
+package in.company.network;
+
+import in.company.beans.NetworkAdaptor;
+
+public class NetworkService
+{
+	private NetworkAdaptor networkAdaptor;
+	
+	
+	public NetworkService(NetworkAdaptor networkAdaptor, String ipAddress) {
+		super();
+		this.networkAdaptor = networkAdaptor;
+		this.ipAddress = ipAddress;
+	}
+	
+	private String ipAddress;
+		
+	
+	public void getServiceConn(String serviceName) {
+		System.out.println("Hey !!!\n"+ serviceName + ""
+				+ " is connected to "+networkAdaptor.getPortIp() +
+				  " with ip address "+ ipAddress);
+	}
+}
