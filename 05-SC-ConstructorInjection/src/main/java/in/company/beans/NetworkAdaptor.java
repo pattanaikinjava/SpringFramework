cb0110506649c3ae35a67832ce826feb01abfe9c
pattanaikinjava <pattanaikd1998@gmail.com>
2023-03-06 07:52:11 +0530

    constructor injection

---
+++ 05-SC-ConstructorInjection/src/main/java/in/company/beans/NetworkAdaptor.java
@@ -0,0 +1,16 @@
+package in.company.beans;
+
+public class NetworkAdaptor {
+	
+	private Integer portIp;
+
+	public NetworkAdaptor(Integer portIp) {
+		this.portIp = portIp;
+	}
+
+	public Integer getPortIp() {
+		return portIp;
+	}
+	
+	
+}
